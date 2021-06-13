72073b4fd7b239d131c829422dda77fe982dc137
mjmhtjain <42150844+mjmhtjain@users.noreply.github.com>
2021-06-13 12:35:06 +0530

    logging package

---
+++ src/logger/logLevel.go
@@ -0,0 +1,18 @@
+package logger
+
+import (
+	"log"
+	"os"
+)
+
+var (
+	WARN  *log.Logger
+	INFO  *log.Logger
+	ERROR *log.Logger
+)
+
+func init() {
+	INFO = log.New(os.Stderr, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
+	WARN = log.New(os.Stderr, "WARNING: ", log.Ldate|log.Ltime|log.Lshortfile)
+	ERROR = log.New(os.Stderr, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
+}
